eea935dbfb90d29a533471c2e6f525870ac775da
Will Smith <wismith@davidson.edu>
2020-04-20 20:08:13 -0500

    change to app.js

--- app.js
+++ app.js
@@ -59,4 +59,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 module.exports = app;
