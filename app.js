c609e1604417cd06dbda524e2dc97328f565f91f
Jesse Farmer <jesse@20bits.com>
2020-04-22 13:27:53 -0700

    Enable extended body parsing in express
    See https://expressjs.com/en/api.html#express.urlencoded
    
    Setting extended to true enables structured querystring parsing
    using the qs library.
    
    https://www.npmjs.com/package/qs#readme

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.engine('hbs', handlebars({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(app.root('public')));
 console.log('DIRNAME: ', __dirname);
